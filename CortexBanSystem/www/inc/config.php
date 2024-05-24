95cdb6e72a03079532289b015a9907e01e9f00f9
mIDnight <42657432+EVOLCORE@users.noreply.github.com>
2024-05-24 21:07:04 +0200

    Update config.php

--- CortexBanSystem/www/inc/config.php
+++ CortexBanSystem/www/inc/config.php
@@ -1,23 +1,29 @@
 <?php
-
 $db_credentials = [
     'host' => "localhost",
-    'user' => "",
+    'user' => "root",
     'password' => "",
     'name' => "",
 ];
 
 try {
-    // Create a PDO connection
     $dsn = "mysql:host={$db_credentials['host']};dbname={$db_credentials['name']}";
     $conn = new PDO($dsn, $db_credentials['user'], $db_credentials['password']);
-
-    // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Log the error or handle it appropriately
     error_log("Connection failed: " . $e->getMessage());
     exit("An error occurred. Please try again later.");
 }
 
+// Define IPHub API keys
+$iphub_api_keys = [
+    'MjQwMTk6WnM2Q0dneVZEREt3Yk1DYjA1MlBTalZvMDJrVVJTV2Q=',
+    'MjQwMjA6RlR4YUNzekdMTWIzTFljRUlXOUdzcktRTlkxOUx5d1M=',
+    'MTAzNTQ6TGgwSUhpZmRCS3BaVUEwaVY3bWhhUDcxbWpZQkMyZ2o=',
+    'MjQwMjE6UGpVbnd6bkVXNlY1d3drV1lwVThDbGdtS3F1NGhqY3I='
+];
+
+// Define table name
+$table_check = "db_ccheck";
+$ban_table = "cortex_bans"
 ?>
